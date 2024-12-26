987d9e9ff2852ead8d63fe8bcd46b376e5a39351
Aleksei Zabrodskii <elmigranto@protonmail.com>
2024-12-26 15:37:25 +0100

    Xcode's "New Project"

---
+++ AoC-iOS.swift/AoC_iOS_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  AoC_iOS_swiftApp.swift
+//  AoC-iOS.swift
+//
+//  Created by Aleksei Zabrodskii on 26/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct AoC_iOS_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AoC-iOS.swiftTests/AoC_iOS_swiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  AoC_iOS_swiftTests.swift
+//  AoC-iOS.swiftTests
+//
+//  Created by Aleksei Zabrodskii on 26/12/2024.
+//
+
+import Testing
+@testable import AoC_iOS_swift
+
+struct AoC_iOS_swiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
